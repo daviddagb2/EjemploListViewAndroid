5a138c0656962962c73262c2907e2dab251482fd
David Gonzalez Blanchard <daviddagb2@gmail.com>
2022-03-04 15:56:19 -0600

    Se añade clases para el ejemplo de listview

--- app/src/main/java/com/gonzalez/blanchard/ejemplolistviewviernes/MainActivity.java
+++ app/src/main/java/com/gonzalez/blanchard/ejemplolistviewviernes/MainActivity.java
@@ -2,14 +2,73 @@ package com.gonzalez.blanchard.ejemplolistviewviernes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listview;
+    private ArrayList<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = findViewById(R.id.listview1);
+
+        names = new ArrayList<String>();
+        names.add("Iron Man");
+        names.add("Capitan America");
+        names.add("Viuda Negra");
+        names.add("Vision");
+        names.add("Hulk");
+        names.add("Thor");
+        names.add("Doctor Strange");
+        names.add("Nick Fury");
+        names.add("Capitana Marvel");
+        names.add("Scarleth With");
+        names.add("Thanos");
+        names.add("Soy inevitable");
+        names.add("Y yo soy Iron Man");
+        names.add("Iron Man");
+        names.add("Capitan America");
+        names.add("Viuda Negra");
+        names.add("Vision");
+        names.add("Hulk");
+        names.add("Thor");
+        names.add("Pantera blanca");
+        names.add("Nick Fury");
+        names.add("Capitana Marvel");
+        names.add("Scarleth With");
+        names.add("Thanos");
+        names.add("Soy inevitable");
+        names.add("Pantera negra");
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, names);
+
+        listview.setAdapter(adapter);
+
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Has seleccionado" + names.get(i), Toast.LENGTH_SHORT).show();
+
+                Intent pantalla2 = new Intent(MainActivity.this, DetallePersonaje.class);
+                startActivity(pantalla2);
+
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/com/gonzalez/blanchard/ejemplolistviewviernes/Personaje.java
@@ -0,0 +1,11 @@
+package com.gonzalez.blanchard.ejemplolistviewviernes;
+
+public class Personaje {
+
+    public int id;
+    public String nombre;
+    public boolean estaVivo = true;
+    public String foto = "";
+    public String descripcion;
+
+}
